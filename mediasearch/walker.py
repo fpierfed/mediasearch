8af94af25f0c9207c05cb4502b7016458e2c9549
Francesco Pierfederici <fpierfed@gmail.com>
2026-06-30 14:52:29 +0200

    better handling of hidden files and directories, deduplication

--- mediasearch/walker.py
+++ mediasearch/walker.py
@@ -18,21 +18,43 @@ def walk(roots: list[Path]) -> Iterator[MediaFile]:
     We intentionally follow symlinks, so media files reached through a symlink
     (even one pointing outside the root) are intentionally included.  Broken
     symlinks or unreadable files are skipped via OSError.
+
+    To note:
+        * we do not process files or directories whose name starts with '.'
+        * we keep track of duplicates, so that a lnk resolves to the same
+          file, we do not process it twice.
     """
+    seen: set[Path] = set()
+
+    # This might be controversial: we skip files and directories whose name
+    # starts with '.', UNLESS it is a directory in roots. The idea here is
+    # that if the user asked to process a "hidden" dir, we should really do it
     for root in roots:
         root = Path(root).resolve()
-        for dirpath, _, filenames in root.walk(
+        for dirpath, dirnames, filenames in root.walk(
             on_error=print, follow_symlinks=True
         ):
-            # We skip directories and files whose name starts with '.'
-            if dirpath.name.startswith('.'):
+            # resolve symlinks at the dirpath level.
+            dirpath = dirpath.resolve()
+            if dirpath in seen:
                 continue
+            seen.add(dirpath)
+
+            # We skip directories and files whose name starts with '.'
+            # NB: we need to update dirnames in place otherwise it will not
+            # work as intended. Hence the dirnames[:] syntax.
+            dirnames[:] = [n for n in dirnames if not n.startswith('.')]
 
             for filename in filenames:
                 if filename.startswith('.'):
                     continue
 
                 p = dirpath / filename
+                p.resolve()
+                if p in seen:
+                    continue
+                seen.add(p)
+
                 media_type = classify_ext(p)
                 if media_type is None:
                     continue
